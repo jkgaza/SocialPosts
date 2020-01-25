d036e8194f7c97cd59134735f6744a64a97b8d9e
Jordan Gaza <Gaza>
2020-01-25 15:49:11 -0500

    Adding some content to post-form

--- SocialPosts/src/app/social-posts/social-posts.component.ts
+++ SocialPosts/src/app/social-posts/social-posts.component.ts
@@ -15,7 +15,7 @@ export class SocialPostsComponent implements OnInit {
     {title: "I bought a coffee", thought: "I had a hard day and I wanted to. Treat. My. Self."},
     {title: "Did you know", thought: "That celery is super, super gross."},
     {title: "What's the deal??", thought: "Pants are weird. What's up with that?"},
-    {title: "Just noticed", thought: "My hair kind of smells bad."},
+    {title: "Just noticed", thought: "My hair kind of smells like strawberries."},
   ];
 
   constructor() { }
