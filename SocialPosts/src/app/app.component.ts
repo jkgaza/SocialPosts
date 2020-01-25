efcb028ef6fdde0be1facf39ec17c4ec7b4b3814
Jordan Gaza <Gaza>
2020-01-25 16:05:10 -0500

    getting rid of redundant code

--- SocialPosts/src/app/app.component.ts
+++ SocialPosts/src/app/app.component.ts
@@ -10,13 +10,13 @@ export class AppComponent implements OnInit {
   @Input() thought: string;
   // title = 'SocialPosts';
 
-  posts = [
-    {title: "Saw a dog today", thought: "It was super neat."},
-    {title: "I bought a coffee", thought: "I had a hard day and I wanted to. Treat. My. Self."},
-    {title: "Did you know", thought: "That celery is super, super gross."},
-    {title: "What's the deal??", thought: "Pants are weird. What's up with that?"},
-    {title: "Just noticed", thought: "My hair kind of smells bad."},
-  ];
+  // posts = [
+  //   {title: "Saw a dog today", thought: "It was super neat."},
+  //   {title: "I bought a coffee", thought: "I had a hard day and I wanted to. Treat. My. Self."},
+  //   {title: "Did you know", thought: "That celery is super, super gross."},
+  //   {title: "What's the deal??", thought: "Pants are weird. What's up with that?"},
+  //   {title: "Just noticed", thought: "My hair kind of smells bad."},
+  // ];
 
   constructor(){
 
@@ -26,7 +26,7 @@ export class AppComponent implements OnInit {
 
   }
 
-  onDeleted(index: number){
-    this.posts.splice(index, 1);
-  }
+  // onDeleted(index: number){
+  //   this.posts.splice(index, 1);
+  // }
 }
