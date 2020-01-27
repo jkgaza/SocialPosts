62dd97009686513817b60d501338b49e53878324
Jordan Gaza <Gaza>
2020-01-27 18:21:16 -0500

    Working on the modal

--- SocialPosts/src/app/post-form/post-form.component.ts
+++ SocialPosts/src/app/post-form/post-form.component.ts
@@ -5,11 +5,40 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './post-form.component.html',
   styleUrls: ['./post-form.component.css']
 })
+
 export class PostFormComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit() {
-  }
+    
 
-}
+    
+//     // Get the modal
+// let modal = document.getElementById("myModal");
+
+// // Get the button that opens the modal
+// let btn = document.getElementById("myBtn");
+
+// // Get the <span> element that closes the modal
+// let span : Element = document.getElementsByClassName("close")[0];
+
+// // When the user clicks on the button, open the modal
+// btn.onclick = function() {
+//   modal.style.display = "block";
+// }
+
+// // When the user clicks on <span> (x), close the modal
+// span.onclick = function() {
+//   modal.style.display = "none";
+// }
+
+// // When the user clicks anywhere outside of the modal, close it
+// window.onclick = function(event) {
+//   if (event.target == modal) {
+//     modal.style.display = "none";
+//   }
+//   }
+
+
+}}
